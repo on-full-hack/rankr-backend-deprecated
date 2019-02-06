f9dd065eecce30e7a2f825837309e22a3d64c435
dawhey <dawhey@gmail.com>
2019-02-06 12:03:11 +0100

    Add boilerplate code

---
+++ src/main/java/pl/on/full/hack/RankrApplication.java
@@ -0,0 +1,14 @@
+package pl.on.full.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankrApplication.class, args);
+	}
+
+}
+
