b2aa9130f56015fa07dfaa9557cc9b1643085a33
Przemoemo <przemover@gmail.com>
2019-03-20 21:51:22 +0100

    Merge pull request #2 from on-full-hack/docker_containers
    Docker container

--- run_container.sh
+++ run_container.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-mvn clean install
-chmod 777 target/rankr*
+#mvn clean install
+#chmod 777 target/rankr*
+docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
-sudo docker stop rankr_container
-sudo docker rm rankr_container
+#sudo docker stop rankr_container
+#sudo docker rm rankr_container
 sudo docker image rm rankr_container --force
 
-sudo docker image build . -t rankr_container
+sudo docker image build -t rankr_container .
 sudo docker run -p 4000:8080 -d rankr_container -name rankr_container
+
+#docker exec -u 0 rankr_container 'rm app.jar'
+
